4a18ab9c216a20a3d50d1b35fee277daa7d76548
Shivam-guptaa <shivambhupeshgupta@gmail.com>
2025-02-28 21:58:44 +0530

    Fifth commit

--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ app.use(cors({
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
+console.log( process.env.FrontPort);
 
 app.use("/auth", userRoutes);
 
